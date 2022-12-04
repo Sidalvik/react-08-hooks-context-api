e458f23365b8888040c6122969bd22b4e97372b8
Alexandr Sidorenkov <alexandr-sidorenkov@yandex.ru>
2022-12-04 19:09:11 +0400

    task-1, task-2 send

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Separator from './Components/Separator/Separator';
-// import UseEffect from './Components/UseEffect/UseEffect';
+import UseEffect from './Components/UseEffect/UseEffect';
 import UseJsonFetch from './Components/UseJsonFetch/UseJsonFetch';
 
 function App() {
   return (
     <div className="App">
       <h1 className=''>Курс React.<br></br>Домашнее задание по&nbsp;теме<br></br>"Hooks & Context API"</h1>
-      {/* <h2 className='mx-auto text-center'>Задание 1. "useEffect (UseEffect)"</h2>
+      <h2 className='mx-auto text-center'>Задание 1. "useEffect (UseEffect)"</h2>
       <div className='task-1' id='task-1'>
         <UseEffect/>
       </div>
-      <Separator/> */}
+      <Separator/>
       <h2 className='mx-auto text-center'>Задание 2. Кастомный хук "useJsonFetch (UseJsonFetch)"</h2>
       <div className='task-2' id='task-2'>
         <UseJsonFetch/>
